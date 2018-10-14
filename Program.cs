491381dda783bdf3008cfd8a5b4dab2b235f732f
0xRF <god@gmail.com>
2018-10-14 21:52:38 +1030

    Init

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+
+namespace Sora
+{
+    class Program
+    {
+        static void Main(string[] args)
+                 => new Program().MainAsync().GetAwaiter().GetResult();
+
+        DiscordSocketClient _client; 
+        public async Task MainAsync()
+        {
+             _client = new DiscordSocketClient();
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+
+            await _client.LoginAsync(TokenType.User, "");
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
+        }
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+
+        private Task LogAsync(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
